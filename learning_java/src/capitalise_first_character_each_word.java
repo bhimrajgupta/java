af0746cd03a4bc35f64e953534a8077ae221cd59
bhimrajgupta <bhimrajgupta@yahoo.com>
2018-08-26 14:02:28 +0530

    Program: Capitalise first character in each word of a string

---
+++ learning_java/src/capitalise_first_character_each_word.java
@@ -0,0 +1,17 @@
+public class capitalise_first_character_each_word {
+
+    public static void main(String[] args){
+
+        String question = "this is what i was asked to convert";
+        StringBuilder answer = new StringBuilder(question.length());
+        String words[] = question.split("\\ ");
+
+        for(int i=0; i<words.length; i++){
+            answer.append(Character.toUpperCase(words[i].charAt(0)))
+                    .append(words[i].substring(1))
+                    .append(" ");
+        }
+
+        System.out.println(answer);
+    }
+}
