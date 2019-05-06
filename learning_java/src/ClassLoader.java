48470b66057d4bd75c64ae71031fa9d79c426c0d
bhimrajgupta <bhimrajgupta@yahoo.com>
2019-05-06 08:14:04 +0530

    Classloader

---
+++ learning_java/src/ClassLoader.java
@@ -0,0 +1,11 @@
+
+public class ClassLoader {
+
+	public static void main(String[] args) {
+		Class<ClassLoader> c=ClassLoader.class;
+		System.out.println(c.getClassLoader());
+		System.out.println(String.class.getClassLoader());
+
+	}
+
+}
