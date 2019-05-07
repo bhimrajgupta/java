7f498b6def7f65bb01c8d457fb3002c0553e765c
bhimrajgupta <bhimrajgupta@yahoo.com>
2019-05-07 08:48:09 +0530

    Documentation API

---
+++ Understanding_Java/src/BasicPrograms/DocumentationAPI.java
@@ -0,0 +1,15 @@
+package BasicPrograms;
+
+/** Let us test the Documentation API */
+public class DocumentationAPI {
+	/** This add method returns the addition of 2 numbers */
+	public static int add(int a, int b) {
+		return a + b;
+	}
+
+	/** This sub method returns the subtraction of 2 numbers */
+	public static int sub(int a, int b) {
+		return a - b;
+	}
+
+}
